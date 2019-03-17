45481310d8b7b86d3dac20097fa73f39f56089d0
duduvs <eduardotelecio@gmail.com>
2019-03-17 12:41:56 -0300

    adminrepo deleted

--- src/main/java/com/animes/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.animes.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.animes.domain.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Integer> {
-
-}
